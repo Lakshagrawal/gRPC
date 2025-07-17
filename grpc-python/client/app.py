e267187f95e6a22eeacfc0f5960aa11f77f6c2ef
Lakshya Agrawal <lakshya.agrawal@tarento.com>
2025-07-17 18:38:37 +0530

    add the code for the stream in the client and server

--- grpc-python/client/app.py
+++ grpc-python/client/app.py
@@ -10,7 +10,24 @@ def run():
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = users_pb2_grpc.UsersStub(channel)
         response = stub.GetUser(users_pb2.UserRequest(name="Lakshya Agrawal"))
-    print("Greeter client received: " + response.message)
+        print("Greeter client received: " + response.message)
+
+        # print("Getting user stream reply**********************")
+        # response = stub.GetUserStreamReply(users_pb2.UserRequest(name="Lakshya Agrawal"))
+        # for response in response:
+        #     print("Greeter client received: " + response.message)
+
+        print("Getting user bidi stream**********************")
+        # Create an iterator of requests for bidirectional streaming
+        def request_iterator():
+            for i in range(3):  # Send 3 requests
+                yield users_pb2.UserRequest(name=f"Lakshya Agrawal {i+1}")
+        
+        responses = stub.GetUserBidiStream(request_iterator())
+        for response in responses:
+            print("Greeter client received: " + response.message)
+            
+            
 
 
 if __name__ == "__main__":
--- grpc-python/server/app.py
+++ grpc-python/server/app.py
@@ -1,5 +1,7 @@
 from concurrent import futures
 import logging
+import random
+import time
 
 import grpc
 import users_pb2
@@ -10,8 +12,23 @@ class Users(users_pb2_grpc.UsersServicer):
     def GetUser(self, request, context):
         print("client requested for user: ", request.name)
         return users_pb2.UserReply(message="Hello, %s!" % request.name)
+    
+    def GetUserStreamReply(self, request, context):
+        # print("client requested for user: ", request.name)
+        # return users_pb2.UserReply(message="Hello, %s!" % request.name)
+        while True:
+            temperature = random.uniform(20.0, 30.0)
+            print("client requested for user: ", request.name)
+            yield users_pb2.UserReply(message="Hello, %s!, temperature: %s" % (request.name, temperature))
+            time.sleep(5)
+    
+    def GetUserBidiStream(self, request_iterator, context):
+        for request in request_iterator:
+            print("client requested for user: ", request.name)
+            yield users_pb2.UserReply(message="Hello, %s!" % request.name)
+            time.sleep(5)
 
-
+    
 def serve():
     port = "50051"
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
